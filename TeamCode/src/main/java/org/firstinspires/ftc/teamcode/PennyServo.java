1b8c80dd359b9b8be44e4e3dec6777dc00a044c4
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2025-08-31 15:18:49 -0500

    Setup for base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PennyServo.java
@@ -0,0 +1,102 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+/*
+ * This OpMode executes a POV Game style Teleop for a direct drive robot
+ * The code is structured as a LinearOpMode
+ *
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the arm using the Gamepad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="ServoTesting", group="Robot")
+
+public class PennyServo extends LinearOpMode {
+
+    public Servo ServoClaw;
+
+    public static final double MID_SERVO = 1.0;
+    public static final double ARM_UP_POWER = 0.45;
+    public static final double ARM_DOWN_POWER = -0.45;
+    double ClawPos = 1.0;
+
+    @Override
+    public void runOpMode() {
+
+
+        ServoClaw = hardwareMap.get(ServoImplEx.class, "servo_claw");
+
+
+        // Set motor directions
+        telemetry.addData(">", "Robot Ready. Press START.");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            double SlideDownSpeed = gamepad1.left_trigger;
+            double SlideSpeed = gamepad1.right_trigger;
+            double HoldPower = 0.1;//adjust later
+
+            if (gamepad1.dpad_up && getRuntime() > 0.2){
+                ClawPos += 0.01;
+                resetRuntime();
+            }
+            else if (gamepad1.dpad_down && getRuntime() > 0.2){
+                ClawPos -= 0.01;
+                resetRuntime();
+            }
+            ClawPos = Range.clip(ClawPos, 0.0, 1.0);//clips after the inputs
+            ServoClaw.setPosition(ClawPos);
+
+
+            telemetry.addData("ClawPos", "%.2f", ClawPos);
+            telemetry.update();
+
+            sleep(50);
+        }
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class helpers {
+
+    public static void setDrivePowers(
+            DcMotor FL, DcMotor FR, DcMotor BL, DcMotor BR,
+            double fl, double fr, double bl, double br
+    ) {
+        FL.setPower(fl);
+        FR.setPower(fr);
+        BL.setPower(bl);
+        BR.setPower(br);
+    }
+
+    public static void BasicTele(
+            DcMotor FL, DcMotor FR, DcMotor BL, DcMotor BR,
+            double straight, double strafe, double turn,
+            boolean strafe_right, boolean strafe_left,
+            boolean up, boolean down, boolean speedController
+    ) {
+        double y = -straight;
+        double x = strafe;
+        double rx = turn;
+
+        double power = Math.hypot(x, y);
+        double theta = Math.atan2(y, x);
+
+        double sin45 = Math.sin(theta - Math.PI / 4);
+        double cos45 = Math.cos(theta - Math.PI / 4);
+
+        double max = Math.max(Math.abs(sin45), Math.abs(cos45));
+
+        double flPower = (power * cos45 / max) + rx;
+        double frPower = (power * sin45 / max) - rx;
+        double blPower = (power * sin45 / max) + rx;
+        double brPower = (power * cos45 / max) - rx;
+
+        double denom = power + Math.abs(rx);
+        if (denom > 1.0) {
+            flPower /= denom;
+            frPower /= denom;
+            blPower /= denom;
+            brPower /= denom;
+        }
+
+        double Max = Math.max(Math.abs(blPower), Math.max(Math.abs(brPower), Math.max(Math.abs(flPower), Math.abs(frPower))));
+        if (max > 1.0) {
+            flPower /= Max;
+            frPower /= Max;
+            blPower /= Max;
+            brPower /= Max;
+        }
+
+
+        if (speedController){
+            if (strafe_right) {
+                setDrivePowers(FL, FR, BL, BR, 0.85, -0.85, -0.85, 0.85);
+            } else if (strafe_left) {
+                setDrivePowers(FL, FR, BL, BR, -0.85, 0.85, 0.85, -0.85);
+            } else if (up) {
+                setDrivePowers(FL, FR, BL, BR, 0.85, 0.85, 0.85, 0.85);
+            } else if (down) {
+                setDrivePowers(FL, FR, BL, BR, -0.85, -0.85, -0.85, -0.85);
+            } else {
+                setDrivePowers(
+                        FL, FR, BL, BR,
+                        Math.pow(flPower, 3),
+                        Math.pow(frPower, 3),
+                        Math.pow(blPower, 3),
+                        Math.pow(brPower, 3)
+                );
+            }
+
+        } else {
+            if (strafe_right) {
+                setDrivePowers(FL, FR, BL, BR, 0.55, -0.55, -0.55, 0.55);
+            } else if (strafe_left) {
+                setDrivePowers(FL, FR, BL, BR, -0.55, 0.55, 0.55, -0.55);
+            } else if (up) {
+                setDrivePowers(FL, FR, BL, BR, 0.55, 0.55, 0.55, 0.55);
+            } else if (down) {
+                setDrivePowers(FL, FR, BL, BR, -0.55, -0.55, -0.55, -0.55);
+            } else {
+                setDrivePowers(
+                        FL, FR, BL, BR,
+                        Math.pow(flPower * 0.7, 3),
+                        Math.pow(frPower * 0.7, 3),
+                        Math.pow(blPower * 0.7, 3),
+                        Math.pow(brPower * 0.7, 3)
+                );
+            }
+        }
+    }
+}
